b248cab07bbe2ae1977b82ae96a4bf8a28b37495
dathoang9797 <dathoang9797@gmail.com>
2021-12-21 10:12:04 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-function a(){
-    return 10;
-}
+const logoText = document.querySelector(".header__content .header__logo span");
+
+logoText.style.color = "red";
+
+logoText.innerText = "Hello Cybersoft";
+
+const aTagsNav = document.querySelector(".header__content nav ul a");
+console.log(aTagsNav);
+
+const ulNav = document.querySelector(".header__content ul");
+const btnEl = document.createElement("button");
+btnEl.innerText = "Đăng xuất";
+ulNav.appendChild(btnEl);
+btnEl.className = "btnDangXuat";
+
+const carouselEl = document.querySelector(".carousel");
+carouselEl.classList.add("container");
